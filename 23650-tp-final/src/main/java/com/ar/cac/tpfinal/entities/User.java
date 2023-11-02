70caa6b4380baa81bfef114039a3206e1d832b5a
CrisGadea <cristianhernangadea@gmail.com>
2023-11-02 12:18:34 -0300

    Clase 2-11

--- 23650-tp-final/src/main/java/com/ar/cac/tpfinal/entities/User.java
+++ 23650-tp-final/src/main/java/com/ar/cac/tpfinal/entities/User.java
@@ -1,5 +1,22 @@
 package com.ar.cac.tpfinal.entities;
 
-//@Entity
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usuario")
+    private String username;
+
+    @Column(name = "contrasenia")
+    private String password;
 }
--- 23650-tp-final/src/main/java/com/ar/cac/tpfinal/services/UserService.java
+++ 23650-tp-final/src/main/java/com/ar/cac/tpfinal/services/UserService.java
@@ -1,8 +1,40 @@
 package com.ar.cac.tpfinal.services;
 
+import com.ar.cac.tpfinal.entities.User;
+import com.ar.cac.tpfinal.entities.dtos.UserDto;
+import com.ar.cac.tpfinal.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
 
-}
+    @Autowired
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository){
+        this.repository = repository;
+    }
+
+    public List<User> getUsers(){
+        List<User> users = repository.findAll();
+        return users;
+    }
+
+    public User getUserById(Long id){
+        User user = repository.findById(id).get();
+        return user;
+    }
+
+    public UserDto createUser(UserDto user){
+        //List<String> users = this.getUsers();
+        //users.add(user.getUsername());
+        return user;
+    }
+
+
+    }
